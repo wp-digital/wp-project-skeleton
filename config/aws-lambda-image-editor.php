c303685f3c649ac9da529cbf667bb021603ebf58
Viktor Kuliebiakin <kuliebiakin@gmail.com>
2021-03-29 20:13:47 +0300

    update normalize

--- config/aws-lambda-image-editor.php
+++ config/aws-lambda-image-editor.php
@@ -3,11 +3,12 @@
 use Innocode\WPConfig\Env;
 use Innocode\WPConfig\Helpers;
 
-define( 'AWS_LAMBDA_IMAGE_BUCKET', Env::get(
-    Helpers::is_s3_uploads_enabled()
-        ? 'S3_UPLOADS_BUCKET'
-        : 'AWS_LAMBDA_IMAGE_BUCKET'
-) );
+if ( Env::get( 'AWS_LAMBDA_IMAGE_BUCKET' ) ) {
+    define( 'AWS_LAMBDA_IMAGE_BUCKET', Env::get( 'AWS_LAMBDA_IMAGE_BUCKET' ) );
+} elseif ( Helpers::is_s3_uploads_enabled() ) {
+    define( 'AWS_LAMBDA_IMAGE_BUCKET', Env::get( 'S3_UPLOADS_BUCKET' ) );
+}
+
 define( 'AWS_LAMBDA_IMAGE_KEY', Env::get( 'AWS_LAMBDA_IMAGE_KEY' ) );
 define( 'AWS_LAMBDA_IMAGE_SECRET', Env::get( 'AWS_LAMBDA_IMAGE_SECRET' ) );
 define( 'AWS_LAMBDA_IMAGE_REGION', Env::get( 'AWS_LAMBDA_IMAGE_REGION' ) );
