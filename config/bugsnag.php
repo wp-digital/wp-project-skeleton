d149e40df909db4d1eab45162596caf35775e3dc
Viktor Kuliebiakin <kuliebiakin@gmail.com>
2022-07-28 18:02:25 +0300

    add constants for Bugsnag FE

--- config/bugsnag.php
+++ config/bugsnag.php
@@ -3,3 +3,8 @@
 use Innocode\WPConfig\Env;
 
 define( 'BUGSNAG_API_KEY', Env::get( 'BUGSNAG_API_KEY' ) );
+
+if ( Env::get( 'BUGSNAG_FE_API_KEY' ) ) {
+	define( 'BUGSNAG_FE_API_KEY', Env::get( 'BUGSNAG_FE_API_KEY' ) );
+	define( 'BUGSNAG_FE_VERSION', Env::get( 'BUGSNAG_FE_VERSION', 'v7' ) );
+}
